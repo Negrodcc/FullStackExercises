13d20cd692ec6ec96783bebee09bc3c348686219
Negrodcc <bastian.corrales@ug.uchile.cl>
2024-12-24 17:38:55 -0300

    exercise 1.5 done, i used reduce function, which is a foldr

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -47,33 +47,39 @@ const Total = ({total}) => {
   );
 }
 
+//totalExercises recieved the parts which it´s an array with objects with a exercises field
+// and sum all of them, and by using 'reduce' does'nt matter how many items are in the array
+const totalExercises = (parts) => {
+  return parts.reduce((acc, item) => acc + item.exercises, 0)
+}
 
 const App = () => {
   //Variables keeps in the App Module
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  //for render the Content module, we need to pass the parts array with the 3 SingleContent
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ];
-  // Add a unique id to each item in the parts array, to optimize the map function in the Content module
-  const partsWithId = parts.map((item) => ({
-    ...item,
-    id: uuidv4()
-  }));  
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id : uuidv4()
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id : uuidv4()
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id : uuidv4()
+      }
+    ]
+  }
   return (
     <div>
-      <Header course = {course} />
-      <Content parts = {partsWithId} />
-      <Total total = {exercises1 + exercises2 + exercises3} />
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+      <Total total = {totalExercises(course.parts)} />
     </div>
   )
 }
